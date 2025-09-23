a97e99cc6177af318cfd6879bbce78dcf6dc04ad
Mohammed Zareef-Mustafa <130800056+mzareefmustafa@users.noreply.github.com>
2025-09-22 17:18:46 -0700

    Made a few DE changes

--- server/app.py
+++ server/app.py
@@ -198,5 +198,5 @@ def validate_verification_code():
 
 
 
-if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)), debug=True)
